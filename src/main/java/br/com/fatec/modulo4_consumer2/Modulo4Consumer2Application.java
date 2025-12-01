cc6a9bfe9d1ca011746662e52acc86410d496d82
juliosn <juliodsneves@hotmail.com>
2025-11-30 21:23:01 -0300

    feat: initializing the project

---
+++ src/main/java/br/com/fatec/modulo4_consumer2/Modulo4Consumer2Application.java
@@ -0,0 +1,13 @@
+package br.com.fatec.modulo4_consumer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modulo4Consumer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Modulo4Consumer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/modulo4_consumer2/Modulo4Consumer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.modulo4_consumer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modulo4Consumer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
